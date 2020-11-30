db126d6fd2b9b66c53257d5ce1207c535102ece3
ben <wbpiggot@gmail.com>
2020-11-30 15:46:09 -0800

    updating dashboard mini-app

---
+++ sections/dashboard/src/components/profile.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from "gatsby";
+import { useIdentityContext } from "react-netlify-identity";
+
+const Profile = ({ showModal }) => {
+  const identity = useIdentityContext();
+  const isLoggedIn = identity && identity.isLoggedIn;
+
+  const name = 
+    identity &&
+    identity.user &&
+    identity.user.user_metadata &&
+    identity.user.user_metadata.full_name;
+
+
+
+  return (
+    
+      isLoggedIn && (
+      <div className="dashboard-header">
+        <nav>
+          <Link to="/dashboard/secret" activeClassName="active">
+            Secret stuff
+        </Link>
+          <Link to="/dashboard/base" activeClassName="active">
+            See your base
+        </Link>
+        </nav>
+        <span>Logged in as {name}. <button onClick={showModal}></button></span>
+      </div>
+    )
+  )
+}
+
+export default Profile;
